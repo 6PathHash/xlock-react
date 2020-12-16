0fa238dc5265ea9946ccdfee004a22ce649d904b
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2020-12-16 17:00:41 +0100

    refactor: update README

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-function ipfsMediaSrc(fileName) {
-  return (
-    "https://cloudflare-ipfs.com/ipfs/QmUK2k4RhPjKbqNEUqS2GUwFiSRC2Y1nz6YQ7GvdoN6X1R/media/" +
-    fileName
-  );
-}
-
-export default ipfsMediaSrc;
