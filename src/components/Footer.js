b486f373df4854c43ecda8066601fab5ca5583b0
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2020-12-16 17:22:39 +0100

    feat: add GitHub link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import Telegram from '../assets/telegram.svg'
+import GitHub from '../assets/github.svg'
 import Uniswap from '../assets/uniswap.svg'
 
 function Footer() {
@@ -7,8 +8,11 @@ function Footer() {
       <a target="_blank" rel="noreferrer noopener" href="https://t.me/LIDProtocol">
         <img src={Telegram} alt="LID Protocol Telegram"/>
       </a>
+      <a target="_blank" rel="noreferrer noopener" href="https://github.com/Lid-Protocol/ulock-react">
+        <img src={GitHub} alt="xLock on GitHub"/>
+      </a>
       <a target="_blank" rel="noreferrer noopener" href="https://info.uniswap.org/pair/0xc822d85d2dcedfaf2cefcf69dbd5588e7ffc9f10">
-        <img src={Uniswap} alt="LID Protocol xLock on Uniswap"/>
+        <img src={Uniswap} alt="xLock on Uniswap"/>
       </a>
     </footer>
   );
